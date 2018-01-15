c0b0532e6722189e820d5afee94c8f604883732f
unknown <you@example.com>
2018-01-15 19:28:07 +0800

    计算坐标

--- src/components/Main.js
+++ src/components/Main.js
@@ -98,14 +98,68 @@ imageDatas=(function genImageUrl(imageDataArr){
 })(imageDatas);
 
 // imageDatas = genImageUrl(imageDatas);
+class ImgFigure extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <figure className="img-figure">
+        <img src={this.props.data.imageURL} alt={this.props.data.title}/>
+        <figcaption>
+          <h3 className="img-title">{this.props.data.title}</h3>
+        </figcaption>
+      </figure>
+    );
+  }
+}
+
 
 class AppComponent extends React.Component {
+  constructor(props){
+    super(props);
+    this.state.Constant={
+      centerPos:{
+        left:0,
+        reght:0
+      },
+      hPosRange:{
+        leftSecX:[0,0],
+        rightSecX:[0,0],
+        y:[0,0]
+      },
+      vPosRange:{
+        x:[0,0],
+        top:[0,0]
+      }
+    }
+  }
+  componentDidMount(){
+      let stageDOM = this.refs.stage,
+          stageW = stageDOM.scrollWidth,
+          stageH = stageDOM.scrollHeight,
+          halfStageW = Math.ceil(stageW/2),
+          halfStageH = Math.ceil(stageH/2);
+      let imgFigureDOM = this.refs.imgFigure0,
+          imgW = imgFigureDOM.scrollWidth,
+          imgH = imgFigureDOM.scrollHeight,
+          halfImgW = Math.ceil(imgW/2),
+          halfImgH= Math.ceil(imgH/2);
+      this.state.Constant.centerPos={
+          left:halfStageW-halfImgW,
+          top:halfStageH-halfImgH
+      }
+  }
   render() {
+    let controllerUnits=[],
+        imgFigures=[];
+        imageDatas.forEach((value,index)=>{
+            imgFigures.push(<ImgFigure key={index} data={value} ref={'imgFigure'+index}/>);
+        })
     return (
-      <div className="index">
-        <img src={imageDatas[0].imageURL} alt="Yeoman Generator" />
-        <div>hello word</div>
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+      <div className="index" ref="stage">
+        {imgFigures}
+        {/* <img src={imageDatas[0].imageURL} alt="Yeoman Generator" /> */}
       </div>
     );
   }
