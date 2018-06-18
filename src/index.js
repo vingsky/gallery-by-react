e5566e6a28222b39af0a85b5a60d2e3a2c4609b6
wy <215723287@qq.com>
2018-06-18 23:16:54 +0800

    xxxadasdas

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/Main';
-
+// asdasdasdasdas
 // Render the main component into the dom
 ReactDOM.render(<App />, document.getElementById('app'));
